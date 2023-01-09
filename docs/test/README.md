7e6783d0ad3ab1b9ff09eb6c21d8042937c4bfd2
hrystynaa <yaskulskahrystyna@gmail.com>
2023-01-09 17:35:34 +0200

    Add test and change controllers

--- docs/test/README.md
+++ docs/test/README.md
@@ -1,4 +1,172 @@
 # Тестування працездатності системи
 
-*В цьому розділі необхідно вказати засоби тестування, навести вихідні коди тестів та результати тестування.*
+Для тестування системи використано VS Code Thunder Client
+
+## Підключення до серверу
+
+<center>
+
+![](../../images/connection.jpg)
+
+</center>
+
+## GET запит:
+### Усіх команд
+
+<center>
+
+![](../../images/Get.jpg)
+
+</center>
+
+### Команди по id
+
+<center>
+
+![](../../images/Get_by_id.jpg)
+
+</center>
+
+### Команди по неіснуючому id
+
+<center>
+
+![](../../images/Get_by_non-existent_id.jpg)
+
+</center>
+
+## POST запит:
+### Додавання існуючої команди
+
+<center>
+
+![](../../images/Post_with_same_name.jpg)
+
+</center>
+
+### Додавання команди без інформації
+
+<center>
+
+![](../../images/Post_without_inf.jpg)
+
+</center>
+
+### Приклади додавання команд
+
+<center>
+
+![](../../images/Post_ex1.jpg)
+
+![](../../images/Post_ex2.jpg)
+
+![](../../images/Post_ex3.jpg)
+
+</center>
+
+### Результат
+
+<center>
+
+![](../../images/Post_result.jpg)
+
+</center>
+
+## PUT запит:
+### Зміна імені команди
+
+<center>
+
+![](../../images/Put_name.jpg)
+
+</center>
+
+### Результат
+
+<center>
+
+![](../../images/Put_name_result.jpg)
+
+</center>
+
+### Зміна назви списку членів
+
+<center>
+
+![](../../images/Put_memberList.jpg)
+
+</center>
+
+### Результат
+
+<center>
+
+![](../../images/Put_memberList_result.jpg)
+
+</center>
+
+### Зміна id
+
+<center>
+
+![](../../images/Put_id.jpg)
+
+</center>
+
+### Результат
+
+<center>
+
+![](../../images/Put_id_result.jpg)
+
+</center>
+
+### Запит зміни без id
+
+<center>
+
+![](../../images/Put_without_id.jpg)
+
+</center>
+
+### Запит зміни за неіснуючим id
+
+<center>
+
+![](../../images/Put_by_non-existent_id.jpg)
+
+</center>
+
+### Запит зміни без інформації
+
+<center>
+
+![](../../images/Put_without_content.jpg)
+
+</center>
+
+## DELETE запит:
+### Видалення за існуючим id 
+
+<center>
+
+![](../../images/delete_by_id.jpg)
+
+</center>
+
+### Результат
+
+<center>
+
+![](../../images/delete_by_id_result.jpg)
+
+</center>
+
+## Видалення за неіснуючим id
+
+<center>
+
+![](../../images/delete_by_non-existent_id.jpg)
+
+</center>
 
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -20,27 +20,34 @@ const getOneTeam = (req, res) => {
 
 const createNewTeam = (req, res) => {
     const { name, icon, memberList, project_id, member_id } = req.body;
-
-    if (!name) return res.status(400).json({ 'message': 'Name, icon and memberList required' });
-    const query = "INSERT INTO team SET ?";
-    const team = {
-     name,
-     icon,
-     memberList,
-     project_id: project_id || 1,
-     member_id: member_id || 1,
-    };
-  db.query(query, team, (err) => {
-    if (err) return res.status(500).json(err);
-    res.status(201).json({ message: "New team created" });
-  });
+    db.query(`SELECT * FROM team WHERE name="${name}"`, (err, result) =>{
+        if (err) return res.status(500).json(err);
+        if (result.length !== 0) return res.status(406).json('There is already team with this name');
+        if (!name && !icon && !memberList) return res.status(400).json({ 'message': 'Name, icon and memberList required' });
+        const query = "INSERT INTO team SET ?";
+        const team = {
+         name,
+        icon,
+         memberList,
+         project_id: project_id || 1,
+         member_id: member_id || 1,
+        };
+        db.query(query, team, (err) => {
+            if (err) return res.status(500).json(err);
+            res.status(201).json({ message: "New team created" });
+        });
+    });
 };
 
 const updateOneTeam = (req, res) => {
-    const { name, icon, memberList } = req.body;
-    if (!name && !memberList && !icon) return res.status(204).json({ 'message': 'Name, icon or memberList required' });
-
-    let query = '';
+    const { name, icon, memberList, project_id, member_id } = req.body;
+    if (!name && !memberList && !icon && !project_id && !member_id) return res.status(204).json({ 'message': 'Name, icon or memberList required' });
+    
+    db.query(`SELECT * FROM team WHERE id=${req.params.id}`, (err, result) =>{
+        if (err) return res.status(500).json(err);
+        if (result.length === 0) return res.status(404).json('No team with this id');
+    
+        let query = '';
     if (name) {
         query = `UPDATE team SET name = '${name}' WHERE id = '${req.params.id}'`;
         db.query(query, (err) => { if (err) return res.status(500).json(err) });
@@ -53,15 +60,27 @@ const updateOneTeam = (req, res) => {
         query = `UPDATE team SET icon = '${icon}' WHERE id = '${req.params.id}'`;
         db.query(query, (err) => { if (err) return res.status(500).json(err) });
     }
-
+    if (project_id) {
+        query = `UPDATE team SET project_id = '${project_id}' WHERE id = '${req.params.id}'`;
+        db.query(query, (err) => { if (err) return res.status(500).json(err) });
+    }
+    if (member_id) {
+        query = `UPDATE team SET member_id = '${member_id}' WHERE id = '${req.params.id}'`;
+        db.query(query, (err) => { if (err) return res.status(500).json(err) });
+    }
     res.status(200).json({ 'message': 'Team updated' });
+});
 };
 
 const deleteOneTeam = (req, res) => {
     const query = `DELETE FROM team WHERE id=${req.params.id}`;
-    db.query(query, (err, result) => {
+    db.query(`SELECT * FROM team WHERE id=${req.params.id}`, (err, result) => {
         if (err) return res.status(500).json(err);
-        res.status(200).json({ 'message': 'Team deleted' });
+        if (result.length === 0) return res.status(404).json('No team with this id');
+        db.query(query, (err, result) => {
+            if (err) return res.status(500).json(err);
+            res.status(200).json({ 'message': 'Team deleted' });
+        });
     });
 };
 
